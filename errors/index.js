6f25efbbb041fde240c4ddf83c106d33de4d7165
quyenptt03 <quyenptt03@gmail.com>
2023-05-07 21:43:54 +0700

    authencatetion middleware

--- errors/index.js
+++ errors/index.js
@@ -2,10 +2,12 @@ const CustomAPIError = require("./custom-api");
 const UnauthenticatedError = require("./unauthenticated");
 const BadRequestError = require("./bad-request");
 const NotFoundError = require("./not-found");
+const UnauthorizedError = require("./unauthorized");
 
 module.exports = {
   CustomAPIError,
   UnauthenticatedError,
   BadRequestError,
   NotFoundError,
+  UnauthorizedError,
 };
--- index.js
+++ index.js
@@ -3,13 +3,15 @@ require("express-async-errors");
 // express
 const express = require("express");
 const app = express();
-// other packages
+// rest of the packages
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 //database
 const connectDB = require("./db/connectDB");
 // routes
 const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
 //middleware
 const notFoundMiddleware = require("./middleware/not-found.js");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -17,6 +19,7 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello from my app");
@@ -28,6 +31,7 @@ app.get("/api/v1", (req, res) => {
 });
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
